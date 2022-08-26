e75dc840f732240da25a1e02da00443e668d02af
TSYF <tsyf1999@gmail.com>
2022-08-26 13:51:10 -0400

    second first commit lol

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import THeader from '@/components/THeader.vue'
+import TContent from '@/components/TContent.vue';
+</script>
+
+<template>
+	<THeader />
+	<TContent />
+</template>
+
+<style scoped>
+</style>
