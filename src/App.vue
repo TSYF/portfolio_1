26121c69c8c42399bdfa1ab80fa894b4309a9151
TSYF <tsyf1999@gmail.com>
2022-09-05 15:48:01 -0400

    App.vue swapped template and script tags. Structure now is: template -> script -> style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup>
-import THeader from "@/components/THeader.vue";
-import TMain from "./components/TMain.vue";
-</script>
-
 <template>
     <THeader />
     <TMain />
 </template>
+
+<script setup>
+import THeader from "@/components/THeader.vue";
+import TMain from "./components/TMain.vue";
+</script>
 
 <style scoped></style>
