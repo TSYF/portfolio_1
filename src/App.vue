669ed036e23d02961b182d6d40f3206015afac8f
TSYF <tsyf1999@gmail.com>
2022-09-05 16:48:32 -0400

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <THeader />
     <TMain />
+    <TFooter />
 </template>
 
 <script setup>
 import THeader from "@/components/THeader.vue";
-import TMain from "./components/TMain.vue";
+import TMain from "@/components/TMain.vue";
+import TFooter from "@/components/TFooter.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/TFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <a href="mailto:tomas.y@tutanota.com" class="footer__link"
+            >tomas.y@tutanota.com</a
+        >
+        <ul class="social-list">
+            <li class="social-list__item">
+                <a href="https://github.com/TSYF" class="social-list__link"
+                    >icon</a
+                >
+            </li>
+            <li class="social-list__item">
+                <a
+                    href="https://linkedin.com/in/tomas-yanez"
+                    class="social-list__link"
+                    >icon</a
+                >
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
