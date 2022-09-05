2921348ac5b7dce8d931770bb0dd25a5aab4e484
TSYF <tsyf1999@gmail.com>
2022-09-05 16:07:21 -0400

    About Section

---
+++ src/components/TAbout.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="about" id="about">
+        <header>
+            <h2 class="section__title section__title--about">Who I am</h2>
+            <p class="section__subtitle section__subtitle--about"></p>
+        </header>
+
+        <div class="about-me__body">
+            <p></p>
+        </div>
+
+        <img src="" alt="Placeholder Image" />
+    </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/components/TMain.vue
+++ src/components/TMain.vue
@@ -2,12 +2,14 @@
     <main>
         <TIntro />
         <TServices />
+        <TAbout />
     </main>
 </template>
 
 <script setup>
 import TIntro from "./TIntro.vue";
 import TServices from "./TServices.vue";
+import TAbout from "./TAbout.vue";
 </script>
 
 <style lang="scss" scoped></style>
