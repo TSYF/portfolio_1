dcb8a975cd377fc7eb6bb95cdb6c2a224ba2b437
TSYF <tsyf1999@gmail.com>
2022-09-05 15:42:15 -0400

    Set up TMain.vue component for main content

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import THeader from "@/components/THeader.vue";
+import TMain from "./components/TMain.vue";
 </script>
 
 <template>
     <THeader />
+    <TMain />
 </template>
 
 <style scoped></style>
