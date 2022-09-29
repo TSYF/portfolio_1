686d5f2248965f5512f9b4e948809bec2a4c35e0
TSYF <tsyf1999@gmail.com>
2022-09-29 15:34:36 -0300

    Implemented TCard component where placeholder links used to be at TWork section

--- src/components/TWork.vue
+++ src/components/TWork.vue
@@ -2,25 +2,79 @@
     <section class="work" id="work">
         <header>
             <h2 class="section__title section__title--work">My Work</h2>
-            <p class="section__subtitle section__subtitle--work"></p>
+            <p class="section__subtitle section__subtitle--work">
+                A selection of my range of work
+            </p>
         </header>
         <div class="portfolio">
             <a href="#" class="portfolio__item">
-                Placeholder (might put a card in)
+                <TCard
+                    class="portfolio__card"
+                    header="Vue"
+                    bg-color="--color-light-tr"
+                    padding="1em"
+                >
+                    <template #img>
+                        <img
+                            class="card__img"
+                            src="@img/vuejs.png"
+                            alt="Vue Framework"
+                        />
+                    </template>
+                </TCard>
             </a>
             <a href="#" class="portfolio__item">
-                Placeholder (might put a card in)
+                <TCard
+                    class="portfolio__card"
+                    header="Vue"
+                    bg-color="--color-light-tr"
+                    padding="1em"
+                >
+                    <template #img>
+                        <img
+                            class="card__img"
+                            src="@img/vuejs.png"
+                            alt="Vue Framework"
+                        />
+                    </template>
+                </TCard>
             </a>
             <a href="#" class="portfolio__item">
-                Placeholder (might put a card in)
+                <TCard
+                    class="portfolio__card"
+                    header="Vue"
+                    bg-color="--color-light-tr"
+                    padding="1em"
+                >
+                    <template #img>
+                        <img
+                            class="card__img"
+                            src="@img/vuejs.png"
+                            alt="Vue Framework"
+                        />
+                    </template>
+                </TCard>
             </a>
             <a href="#" class="portfolio__item">
-                Placeholder (might put a card in)
+                <TCard
+                    class="portfolio__card"
+                    header="Vue"
+                    bg-color="--color-light-tr"
+                    padding="1em"
+                >
+                    <template #img>
+                        <img
+                            class="card__img"
+                            src="@img/vuejs.png"
+                            alt="Vue Framework"
+                        />
+                    </template>
+                </TCard>
             </a>
         </div>
     </section>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+import TCard from "./TCard.vue";
+</script>
