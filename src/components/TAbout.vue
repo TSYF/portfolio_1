4f0b2163cdd9ec521585e8f8aaf99afdb4dd30e3
TSYF <tsyf1999@gmail.com>
2022-09-28 23:40:50 -0300

    remade markup for the sake of the subtitle bar below image effect. Split body paragraph into two filled them with lorem ipsum,

--- src/components/TAbout.vue
+++ src/components/TAbout.vue
@@ -1,15 +1,31 @@
 <template>
     <section class="about" id="about">
-        <header>
-            <h2 class="section__title section__title--about">Who I am</h2>
-            <p class="section__subtitle section__subtitle--about"></p>
-        </header>
+        <h2 class="section__title section__title--about">Who I am</h2>
+        <p class="section__subtitle section__subtitle--about">
+            Software Dev based in Santiago
+        </p>
 
-        <div class="about-me__body">
-            <p></p>
+        <div class="about__body">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+                libero! Ab repellendus temporibus adipisci explicabo reiciendis,
+                eum recusandae ad voluptatibus! Natus voluptatibus, officia
+                delectus eveniet expedita necessitatibus possimus vel? Hic saepe
+                sint quas? Voluptate, consectetur!
+            </p>
+
+            <p>
+                Ipsa laudantium autem praesentium quas ratione. Voluptates
+                debitis, enim doloribus ipsum porro optio assumenda labore
+                consectetur eum? Ipsa alias molestiae laudantium omnis minus
+                minima laborum eum.
+            </p>
         </div>
-
-        <img src="" alt="Placeholder Image" />
+        <img
+            src="@img/tomas-dev_2.jpeg"
+            alt="Tomas explaining something while looking at the camera man"
+            class="about__img"
+        />
     </section>
 </template>
 
