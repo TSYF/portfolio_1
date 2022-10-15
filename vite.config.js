22d7a9baadc0eb37de29b775caf7038ab12192e1
TSYF <tsyf1999@gmail.com>
2022-10-15 00:21:30 -0300

    defined @store route

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,14 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@": fileURLToPath(new URL("./src/", import.meta.url)),
             "@components": fileURLToPath(
                 new URL("./src/components", import.meta.url)
             ),
             "@img": fileURLToPath(new URL("./src/assets/img", import.meta.url)),
             "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
             "@router": fileURLToPath(new URL("./src/router", import.meta.url)),
+            "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
         },
     },
 });
