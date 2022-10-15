f90890ef65bd4ed068067e2d8f7ac421419d7234
TSYF <tsyf1999@gmail.com>
2022-10-15 00:15:30 -0300

    .use(createPinia()) in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "@router";
 import App from "./App.vue";
 import "@/assets/main.scss";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
